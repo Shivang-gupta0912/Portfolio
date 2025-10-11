2182a8828781a7218762e8225c5d107eef9e04ed
Hanu-unique <hanugupta456@gmail.com>
2025-10-11 15:40:14 +0530

    added projects and contact me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,3 +53,17 @@ tabs.forEach(tab =>{
         tab.classList.add('qualification__active')
     })
 })
+
+// PORTFOLIO(PROJECTS) SWIPER
+let swiperProjects = new Swiper(".projects__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+});
